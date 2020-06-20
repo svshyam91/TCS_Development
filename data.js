f743332c39c4d84f83bf5dc4d71d9e43f5af5122
Shyam Kumar <svshyam111@gmail.com>
2020-06-20 08:06:57 +0530

    Initial Commit

---
+++ data.js
@@ -0,0 +1,12 @@
+var dat = { 
+			fc: {
+				acknowledgement: "We acknowledge your request and we are glad to assist you on the same.\nWe are further analyzing the details provided by you in the ticket and will get back to you with the update shortly.", 
+				pqt_kernel_upgrade: "Process QT:\n\nType of activity:Kernel Upgrade\nExtended SID :\nDB Type ( HANA/ Sybase):\nDowntime Start:\nDowntime End:\nTarget version :\n Risk involved:\nAdditional Information: Kernel files are downloaded in path: ______________\nKindly crosscheck and proceed with the activity.",
+				pqt_parameter_change: "Processor QT:\nType of activity:Parameter Change\nExtended SID :\nSystem:\n\nProfile Change\nRequested:\n\nDowntime Start:\nDowntime End:\n\nRisk involved:\nCautionary Note: Kindly crosscheck the parameters before proceeding with the activity.",
+			}, 
+			ss: {
+				ss_cgs:"<b>Start/Stop Customer Gateway Server</b>\n\nPath(as root): /etc/init.d/\n\nStatus: ./saprouter status\nStop :./saprouter stop\nStart:./saprouter start",
+			},
+		};
+
+var person = {firstName:{fn: "Shyam"}, lastName:"Doe", age:50, eyeColor:"blue"};
